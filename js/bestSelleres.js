9e062c5cc6abf7ad0843466e31d544bc070acc3c
Cristian Otero <cristianoteroadsi@gmail.com>
2022-08-28 12:07:40 -0500

    cargas dinamicas Axios listas

--- js/bestSelleres.js
+++ js/bestSelleres.js
@@ -1,98 +1,64 @@
 (function () {
-  $(document).ready(function () {
-
-    const bestSellers = [
-      {
-        category: 'men',
-        color: '',
-        brand: 'Block Buster',
-        image: './assets/products/best_sellers/men-jacket-RFMJCK01.webp',
-        name: `Leather Jacket Urban`,
-        size: 'L',
-        product: {
-          discount: 0,
-          price: 500,
-          reference: 'RFMJCK01',
-          sale: 0,
-        },
-      },
-      {
-        category: 'men',
-        color: '',
-        brand: 'Levis',
-        image: './assets/products/best_sellers/men-jacket-RFMJCK02.webp',
-        name: `Women's Jersey`,
-        size: 'L',
-        product: {
-          discount: 0,
-          price: 610,
-          reference: 'RFWJ002',
-          sale: 0,
-        },
-      },
-      {
-        category: 'men',
-        color: '',
-        brand: 'Ecko UNLTD',
-        image: '../assets/products/best_sellers/men-tshirt-RFMTSH01.webp',
-        name: `Men's Hoddie`,
-        size: 'L',
-        product: {
-          discount: 50,
-          price: 590,
-          reference: 'RFMH001',
-          sale: 0,
-        },
-      },
-    ];
-    const carousel = document.getElementById('bestSellers_carousel');
-    const fragment = document.createDocumentFragment();
-
-    function cardTemplate(product, card) {
-      const {
-        category,
-        brand,
-        image,
-        name,
-        product: { discount, price, reference, sale },
-      } = product;
-
-      card.className = `product active ${category}`;
-      card.id = `${reference}`;
-      card.innerHTML = `
-      <img src="${image}" alt="${name}"/>
-      <p class="name">${name}</p>
-      <p class="brand"> 
-        Brand - ${brand} 
-      </p>
-      <div class="prices">
-          <span class="current">
-            $${price - discount - (price * sale) / 100}.00
-          </span>
-          ${!discount ? '' : '<span class="discount">' + price + ' 00 </span>'}
-          ${!sale ? '' : '<span class="sale">' + price + ' 00 </span>'}
-      </div>
-      
-      ${!sale ? '' : '<span class="label">SALE -' + sale + '%</span>'}
-      
-      <button class="add-product">ADD TO BAG</button>
-    `;
-
-      return card;
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', './mocks/best-seller.json', true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const productsData = JSON.parse(this.responseText);
+      renderProducts(productsData);
     }
+  };
 
-    for (product of bestSellers) {
+  const carousel = document.getElementById('bestSellers_carousel');
+  const bestSellersFragment = document.createDocumentFragment();
+  function renderProducts(productsData) {
+    for (product of productsData) {
       const card = document.createElement('div');
       const productCard = cardTemplate(product, card);
-      fragment.appendChild(productCard);
+      bestSellersFragment.appendChild(productCard);
     }
+    carousel.appendChild(bestSellersFragment);
+  }
 
-    carousel.appendChild(fragment);
+  function cardTemplate(product, card) {
+    const {
+      category,
+      brand,
+      image,
+      name,
+      product: { discount, price, reference, sale },
+    } = product;
 
+    card.className = `product active ${category}`;
+    card.id = `${reference}`;
+    card.innerHTML = `
+        <img src="${image}" alt="${name}"/>
+        <p class="name">${name}</p>
+        <p class="brand"> 
+          Brand - ${brand} 
+        </p>
+        <div class="prices">
+            <span class="current">
+              $${price - discount - (price * sale) / 100}.00
+            </span>
+            ${
+              !discount ? '' : '<span class="discount">' + price + ' 00 </span>'
+            }
+            ${!sale ? '' : '<span class="sale">' + price + ' 00 </span>'}
+        </div>
+      
+        ${!sale ? '' : '<span class="label">SALE -' + sale + '%</span>'}
+        <button class="add-product">ADD TO BAG</button>
+      `;
+    return card;
+  }
+
+  // Carrousel 
+  $(document).ready(function () {
     $('#bestSellers_carousel').slick({
-      Infinity:true,
-      slidesToShow:4,
-      slidesToScroll:1
+      Infinity: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
     });
   });
 })();
--- js/newArrivals.js
+++ js/newArrivals.js
@@ -1,197 +1,63 @@
 (function () {
-  // const products = [
-  //   {
-  //     category: 'women',
-  //     color: '#FF9B1C',
-  //     brand: 'Block Buster',
-  //     image: './assets/products/wmen-jersey-RFWJ001.webp',
-  //     name: `Women's Jersey`,
-  //     size: 'L',
-  //     product: {
-  //       discount: 0,
-  //       price: 120,
-  //       reference: 'RFWJ001',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'women',
-  //     color: '#D39240',
-  //     brand: 'Block Buster',
-  //     image: './assets/products/wmen-jersey-RFWJ002.webp',
-  //     name: `Women's Jersey`,
-  //     size: 'L',
-  //     product: {
-  //       discount: 0,
-  //       price: 610,
-  //       reference: 'RFWJ002',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'men',
-  //     color: '#4C3240',
-  //     brand: 'Block Buster',
-  //     image: './assets/products/men-hoddie-RFMH001.webp',
-  //     name: `Men's Hoddie`,
-  //     size: 'L',
-  //     product: {
-  //       discount: 50,
-  //       price: 590,
-  //       reference: 'RFMH001',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'men',
-  //     color: '#637872',
-  //     brand: 'Capitals',
-  //     image: './assets/products/men-jersey-RFMJ001.webp',
-  //     name: `Men's Jersey`,
-  //     size: 'L',
-  //     product: {
-  //       discount: 0,
-  //       price: 180,
-  //       reference: 'RFMJ001',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'men',
-  //     color: '#DBA96B',
-  //     brand: 'Capitals',
-  //     image: './assets/products/men-coat-RFMC001.webp',
-  //     name: `Men's Coat`,
-  //     size: 'M',
-  //     product: {
-  //       discount: 0,
-  //       price: 410,
-  //       reference: 'RFMC001',
-  //       sale: 40,
-  //     },
-  //   },
-  //   {
-  //     category: 'accessory',
-  //     color: '#DBA96B',
-  //     brand: 'Block Buster',
-  //     image: './assets/products/usex-zips-RFUS001.webp',
-  //     name: `Low Zips`,
-  //     size: '34',
-  //     product: {
-  //       discount: 0,
-  //       price: 189,
-  //       reference: 'RFUS001',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'accessory',
-  //     color: '#CE7F3C',
-  //     brand: 'Capitals',
-  //     image: './assets/products/accs-wmen_wallet-RFAW001.webp',
-  //     name: `Leather Wallet`,
-  //     size: '',
-  //     product: {
-  //       discount: 0,
-  //       price: 120,
-  //       reference: 'RFAW001',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'accessory',
-  //     color: '#C0CEBE',
-  //     brand: 'Capitals',
-  //     image: './assets/products/accs-sport_bag-RFASB01.webp',
-  //     name: `Sport Bag`,
-  //     size: '',
-  //     product: {
-  //       discount: 0,
-  //       price: 547,
-  //       reference: 'RFASB01',
-  //       sale: 40,
-  //     },
-  //   },
-  //   {
-  //     category: 'accessory',
-  //     color: '#6F571D',
-  //     brand: 'Capitals',
-  //     image: './assets/products/accs-bag-RFACB01.webp',
-  //     name: `Casual Bag`,
-  //     size: '',
-  //     product: {
-  //       discount: 0,
-  //       price: 610,
-  //       reference: 'RFACB01',
-  //       sale: 0,
-  //     },
-  //   },
-  //   {
-  //     category: 'accessory',
-  //     color: '#5B5F7F',
-  //     brand: 'Capitals',
-  //     image: './assets/products/accs-aviator_glasses-RFAAG01.webp',
-  //     name: `Aviator glasses`,
-  //     size: '',
-  //     product: {
-  //       discount: 0,
-  //       price: 520,
-  //       reference: 'RFAAG01',
-  //       sale: 20,
-  //     },
-  //   },
-  // ];
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', './mocks/new-arrivals.json', true);
+  xhttp.send();
 
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const productsData = JSON.parse(this.responseText);
+      renderProducts(productsData);
+    }
+  };
+
+  const arrivalsFragment = document.createDocumentFragment();
   const newArrivals = document.getElementById('arrivals-products');
-  const fragment = document.createDocumentFragment();
+  function renderProducts(productsData) {
+    for (const product of productsData) {
+      const card = document.createElement('div');
+      const productCard = cardTemplate(product, card);
+      arrivalsFragment.appendChild(productCard);
+    }
 
- 
-  for (const product of products) {
-    const card = document.createElement('div');
+    newArrivals.appendChild(arrivalsFragment);
+  }
 
+  function cardTemplate(product, card) {
     const {
       category,
-      color,
       brand,
       image,
       name,
-      product: { price, discount, sale, reference },
+      product: { discount, price, reference, sale },
     } = product;
 
     card.className = `product active ${category}`;
     card.id = `${reference}`;
-
     card.innerHTML = `
       <img src="${image}" alt="${name}"/>
       <p class="name">${name}</p>
       <p class="brand"> 
         Brand - ${brand} 
-        <span style="background-color:${color}"></span>
       </p>
       <div class="prices">
-          <span class="current">$${
-            price - discount - (price * sale) / 100
-          }.00</span>
+          <span class="current">
+            $${price - discount - (price * sale) / 100}.00
+          </span>
           ${!discount ? '' : '<span class="discount">' + price + ' 00 </span>'}
           ${!sale ? '' : '<span class="sale">' + price + ' 00 </span>'}
       </div>
-      
       ${!sale ? '' : '<span class="label">SALE -' + sale + '%</span>'}
-      
       <button class="add-product">ADD TO BAG</button>
     `;
 
-    fragment.appendChild(card);
+    return card;
   }
-
-  newArrivals.appendChild(fragment);
 
   // Selecting Category block
   const womenTab = document.getElementById('women-tab');
   const menTab = document.getElementById('men-tab');
   const accessoriesTab = document.getElementById('accessories-tab');
   const allTab = document.getElementById('all-tab');
-
   const arrivalsProducts = document.querySelectorAll(
     '#arrivals-products .product'
   );
